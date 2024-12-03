767803cf2e815d902e0d402c70357c9bd297291f
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-03 19:24:02 +0300

    Add files via upload

---
+++ WebsiteStatusCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitestatus.website_status_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteStatusCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
